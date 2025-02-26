5dc8c2f326f8a871adcb244619e5268470d4af32
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-02-26 14:34:49 +0100

    Resolved todo in DatabaseManager.ts

--- src/database/DatabaseManager.ts
+++ src/database/DatabaseManager.ts
@@ -1,11 +1,18 @@
-import mongoose from "mongoose";
+import mongoose, { MongooseError } from "mongoose";
 
 class DatabaseManager {
     private readonly URL = `mongodb+srv://${process.env.MONGODB_DATABASE_USER}:${process.env.MONGODB_DATABASE_PASSWORD}@${process.env.MONGODB_DATABASE_URL}/?retryWrites=true&w=majority&appName=Cluster0`; // config?
 
     public async Start() {
-        // TODO -> Error handling
-        await mongoose.connect(this.URL);
+        try {
+            await mongoose.connect(this.URL);
+        } catch (err) {
+            if (err instanceof MongooseError) {
+                throw new Error(`${err.message}, ${err.stack}`);
+            } else if (err instanceof Error) {
+                throw new Error(err.message);
+            }
+        }
     }
 }
 
