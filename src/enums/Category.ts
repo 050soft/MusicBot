82d015d11fee80d6554d68abfe2e12d0b9f9a05e
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-03-13 19:09:30 +0100

    Remove all files

--- src/enums/Category.ts
+++
@@ -1,5 +0,0 @@
-enum Category {
-    Information = "Information",
-}
-
-export default Category;
--- src/interfaces/IEvent.ts
+++
@@ -1,9 +0,0 @@
-import { Events } from "discord.js";
-import Bot from "../bot/Bot";
-
-export default interface IEvent {
-    name: Events | string;
-    once?: boolean;
-
-    execute(bot: Bot, ...args: any): Promise<unknown> | void;
-}
