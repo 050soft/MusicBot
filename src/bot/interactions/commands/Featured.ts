ef0e681a1f3506a83d81b4215cf8a44adafe9375
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-03-13 15:22:02 +0100

    Update Featured.ts

--- src/bot/interactions/commands/Featured.ts
+++ src/bot/interactions/commands/Featured.ts
@@ -11,7 +11,7 @@ export default <ISlashCommand> {
                 thumbnail: track.image[3]["#text"],
              });
         } else {
-            return await interaction.Bot.ErrorEmbed(interaction, "There is currently no featured track");
+            return await interaction.Bot.ErrorEmbed(interaction, "There seems to be no featured track.");
         }
     },
 }
