d89e6405cfd51245edb5a69aa53721cfcaafde82
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-03-14 14:46:25 +0100

    Update SlashCommand.ts

--- src/classes/structures/SlashCommand.ts
+++ src/classes/structures/SlashCommand.ts
@@ -1,1 +1,12 @@
-abstract class SlashCommand {}
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+export default abstract class SlashCommand {
+    public readonly data: SlashCommandBuilder;
+    // -> category?
+    public readonly cooldown?: number;
+    public abstract execute(interaction: ChatInputCommandInteraction): Promise<unknown>;
+
+    constructor(data: SlashCommandBuilder) {
+        this.data = data;
+    }
+}
