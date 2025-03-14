6a3230b89787cd4f7164c41e9c2e027d2f7c325c
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-03-14 14:06:57 +0100

    fixed tsc issue

--- src/classes/Bot.ts
+++ src/classes/Bot.ts
@@ -2,6 +2,8 @@ import { Client, ClientOptions } from "discord.js";
 import Logger from "./Logger";
 import LogLevel from "../enums/LogLevel";
 
+import { version } from "../../package.json";
+
 class Bot extends Client {
     public Logger = new Logger(LogLevel.VERBOSE);
 
