bc3d54d5f6b4bc91532c9251f60637aa366402cf
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-03-14 15:00:53 +0100

    Update Bot.ts

--- src/classes/Bot.ts
+++ src/classes/Bot.ts
@@ -3,6 +3,7 @@ import Logger from "./Logger";
 import LogLevel from "../enums/LogLevel";
 
 import { version } from "../../package.json";
+const currentYear = new Date().getFullYear();
 
 class Bot extends Client {
     public Logger = new Logger(LogLevel.VERBOSE);
@@ -13,6 +14,14 @@ class Bot extends Client {
         this.Logger = new Logger(LogLevel.VERBOSE);
     }
 
+    public get BotVersion(): string {
+        return `${currentYear}.${version}`;
+    }
+
+    public async GetGuildCount(): Promise<number> {
+        // !! when starting to use shards use the shard-wide counting method
+        return this.guilds.cache.size;
+    }
 }
 
 export default Bot;
