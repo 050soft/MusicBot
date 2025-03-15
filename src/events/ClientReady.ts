2ffa2c19647c03e42b9a3ed12fca1c1e7e00e039
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-03-15 12:24:08 +0100

    debug

--- src/events/ClientReady.ts
+++ src/events/ClientReady.ts
@@ -1,4 +1,4 @@
-import { Client, Events } from "discord.js";
+import { Events } from "discord.js";
 import Event from "../classes/structures/Event";
 
 export default class ClientReady extends Event<Events.ClientReady> {
@@ -9,8 +9,5 @@ export default class ClientReady extends Event<Events.ClientReady> {
     public async execute() {
         this.bot.Logger.info(`Logged in as: ${this.bot.user?.tag}!`, "Client");
         this.bot.Logger.info(`Total Guilds: ${await this.bot.GetGuildCount()}`, "Stats");
-
-        this.bot.Logger.error("hi");
-        this.bot.Logger.warn("hi");
     }
 }
