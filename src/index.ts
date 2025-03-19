5dd5b1aabd84c2b7de51e3251213b53a78c7aa98
Hussar <30427577+Hxssar@users.noreply.github.com>
2025-03-19 18:51:29 +0100

    H3 inside joke

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
-import { GatewayIntentBits } from "discord.js";
+import { ActivityType, GatewayIntentBits } from "discord.js";
 import Bot from "./classes/Bot";
+
+let weekDays = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"]
 
 const bot = new Bot({
     intents: [
@@ -11,6 +13,11 @@ const bot = new Bot({
 export default bot;
 
 bot.login(process.env.TOKEN);
+
+bot.once('ready', () => {
+    bot.user!.setActivity('🔥 METAL ' + weekDays[new Date().getDay()] + '? METAL EVERYDAY!', { type: ActivityType.Custom });
+});
+
 //bot.Start();
 
 // process.on ...
