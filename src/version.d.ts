f2720522e67e2108c34824766e4c6786777905b9
Not_H3 <67106546+damger9@users.noreply.github.com>
2025-09-26 19:12:53 +0200

    add license headers

--- src/version.d.ts
+++ src/version.d.ts
@@ -1,3 +1,8 @@
+// 050soft MusicBot - A Discord music bot by 050soft
+// Copyright (C) 2025  050soft
+// Licensed under the GNU Affero General Public License v3 or later.
+// See <https://www.gnu.org/licenses/> for details.
+
 // File that will be created in /build by the version manager.
 // UPLOAD /build to the server running the code; do NOT build it on the server.
 export const version: string;
